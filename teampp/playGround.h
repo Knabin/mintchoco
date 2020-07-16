1bf6eede309051ffa4bce28bf6004e897106cff9
Nabin Kim <knabinn97@gmail.com>
2020-07-16 16:21:26 +0900

    카메라 추가, zorder 업데이트

--- teampp/playGround.h
+++ teampp/playGround.h
@@ -1,10 +1,13 @@
 #pragma once
 #include "gameNode.h"
+#include <Vfw.h>
+#pragma comment(lib, "vfw32.lib")
 
 class playGround : public gameNode
 {
 private:
 	
+
 public:
 	playGround();
 	~playGround();
