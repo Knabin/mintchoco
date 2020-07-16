2ba8679c9186ecb2fb894099b76c5f4ab311b620
Nabin Kim <knabinn97@gmail.com>
2020-07-16 16:27:31 +0900

    jiseop player

--- teampp/playGround.cpp
+++ teampp/playGround.cpp
@@ -17,6 +17,12 @@ HRESULT playGround::init()
 {
 	gameNode::init(true);
 
+	_backGround1 = IMAGEMANAGER->addImage("backGround1", "image/01_backGround.bmp", 2016, 672, true, RGB(255, 0, 255));//인게임 처음 시작시 배경화면
+
+	_player = new player;
+	_player->init();
+	//플레이어 상속할당
+
 	return S_OK;
 }
 
@@ -31,6 +37,8 @@ void playGround::update()
 {
 	gameNode::update();
 
+	_player->update();//플레이어 업데이트
+
 
 }
 
@@ -40,6 +48,10 @@ void playGround::render()
 	PatBlt(getMemDC(), 0, 0, WINSIZEX, WINSIZEY, WHITENESS);
 	//=================================================
 	
+	_backGround1->render(getMemDC(), 0, 20);//인게임 처음 시작시 배경화면
+	_player->render();//플레이어 렌더
+
+	ZORDER->render();
 	//=============================================
 	_backBuffer->render(getHDC(), 0, 0);
 }
--- teampp/playGround.h
+++ teampp/playGround.h
@@ -1,9 +1,14 @@
 #pragma once
 #include "gameNode.h"
+#include "player.h"
+#include "jump.h"
 
 class playGround : public gameNode
 {
 private:
+	player* _player;//플레이어 상속할당
+	jump* _jump;//점프 상속할당
+	image* _backGround1;//처음시작시 나올 배경화면 이미지
 	
 public:
 	playGround();
